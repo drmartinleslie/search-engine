e278ece6841e15a626c3b9114ef15ea608d48c0c
Martin Leslie <mrmartinleslie@gmail.com>
2018-05-28 10:36:27 -0700

    more unit tests for parsing

--- src/test/scala/cloud/leslie/searchengine/InMemoryDocStoreSpec.scala
+++ src/test/scala/cloud/leslie/searchengine/InMemoryDocStoreSpec.scala
@@ -12,22 +12,4 @@ class InMemoryDocStoreSpec extends FlatSpec with Matchers {
     store.getDocsForKeyword("salt") shouldBe Set("1", "3")
     store.getDocsForKeyword("cream") shouldBe Set("2")
   }
-
-  // TODO: Delete or ignore or mark as performance test?
-  "InMemoryDocStore and CoordinatingFunctions.query" should "have fast querying for 10000s of docs" in {
-    val store = new InMemoryDocStore()
-    val indexStart = System.currentTimeMillis()
-    (0 to 49999).foreach { i =>
-      val keywords = List("a" + i, "b" + i%10, "c" + i%100, "d" + i%1000, "e" + i%10000)
-      store.addDocument(i.toString, keywords)
-    }
-    val indexEnd = System.currentTimeMillis()
-    println(s"Indexing 50000 items took ${(indexEnd - indexStart) / 1000.0 } seconds")
-
-    val requestString = "query a1 | (b2 & c52)"
-    val queryStart = System.currentTimeMillis()
-    val response = CoordinatingFunctions.query(store, requestString)
-    val queryEnd = System.currentTimeMillis()
-    println(s"Querying took ${(queryEnd - queryStart) / 1000.0 } seconds")
-  }
 }
--- src/test/scala/cloud/leslie/searchengine/RequestParserSpec.scala
+++ src/test/scala/cloud/leslie/searchengine/RequestParserSpec.scala
@@ -18,18 +18,56 @@ class RequestParserSpec extends FlatSpec with Matchers {
       And(Or(Term("butter"), Term("potato")), Term("salt"))
   }
 
-  it should "fail on malformed string" in {
-    val failure = RequestParser.query.parse("query (butter |")
-    failure shouldBe a[Parsed.Failure[_, _]]
+  it should "parse a conjunction of disjunctions with some strange spaces and alphanumeric lower/upper-case keywords" in {
+    RequestParser.query.parse("query (Butt23er | poTato) &(salt| pepper )").get.value shouldBe
+      And(Or(Term("Butt23er"), Term("poTato")), Or(Term("salt"),  Term("pepper")))
   }
 
-  "index" should "parse an index request" in {
-    RequestParser.index.parse("index 1 butter potato").get.value shouldBe
-      IndexRequest("1", List("butter", "potato"))
+  it should "parse a more complicated nested query" in {
+    RequestParser.query.parse("query (hello & goodbye) | (23 & (78 | world))").get.value shouldBe
+      Or(And(Term("hello"), Term("goodbye")), And(Term("23"), Or(Term("78"), Term("world"))))
   }
 
-  it should "fail on malformed string" in {
-    val failure = RequestParser.index.parse("index butter")
-    failure shouldBe a[Parsed.Failure[_, _]]
+  it should "fail if we leave out a keyword" in {
+    RequestParser.query.parse("query butter |") shouldBe a[Parsed.Failure[_, _]]
   }
+
+  it should "fail if we have mismatched parentheses" in {
+    RequestParser.query.parse("query butter | potato & salt)") shouldBe a[Parsed.Failure[_, _]]
+  }
+
+  it should "fail if we do not have required parentheses" in {
+    RequestParser.query.parse("query butter & potato & salt") shouldBe a[Parsed.Failure[_, _]]
+  }
+
+  it should "fail if we have non-alphanumeric keyword" in {
+    RequestParser.query.parse("query not-butter") shouldBe a[Parsed.Failure[_, _]]
+  }
+
+  "index" should "parse an index request with multidigit id and alphanumeric keywords" in {
+    RequestParser.index.parse("index 1234 butter23 potato").get.value shouldBe
+      IndexRequest("1234", List("butter23", "potato"))
+  }
+
+  it should "parse an index request with single digit id and zero keywords" in {
+    RequestParser.index.parse("index 1").get.value shouldBe
+      IndexRequest("1", Nil)
+  }
+
+  it should "fail on string without integer" in {
+    RequestParser.index.parse("index butter") shouldBe a[Parsed.Failure[_, _]]
+  }
+
+  it should "fail on string with malformed integer" in {
+    RequestParser.index.parse("index 0123 butter") shouldBe a[Parsed.Failure[_, _]]
+  }
+
+  it should "fail on string with non-alphanumeric keyword" in {
+    RequestParser.index.parse("index 123 not_butter") shouldBe a[Parsed.Failure[_, _]]
+  }
+
+  it should "fail on string with missing space" in {
+    RequestParser.index.parse("index123 butter") shouldBe a[Parsed.Failure[_, _]]
+  }
+
 }
